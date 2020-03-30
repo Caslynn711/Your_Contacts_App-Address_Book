6689cadcbda1dc7cdcb0f777b409bd062471cb2c
Cassie Calderon <liah.xoxo@gmail.com>
2020-03-30 00:06:33 -0400

    Initial commit

---
+++ app/src/main/java/com/androidbootcamp/yourcontactsapp_addressbook/MainActivity.java
@@ -0,0 +1,35 @@
+package com.androidbootcamp.yourcontactsapp_addressbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.btnName1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Contact1.class));
+
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.btnName2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Contact2.class));
+
+            }
+        });
+
+    }
+}
